e973cf74bf0e5a741a7b14bd920ec6135fa6c70b
sayeetale <145047252+sayeetale@users.noreply.github.com>
2024-03-05 00:17:12 +0530

    Update TC_003_Guidepage.java

--- src/test/java/Testcase/TC_003_Guidepage.java
+++ src/test/java/Testcase/TC_003_Guidepage.java
@@ -11,14 +11,14 @@ public class TC_003_Guidepage extends start {
 	@Test
 	public void blog_page()
 	{
-		Homepage hp = new Homepage(driver);
-	hp.resourcebutton();
+        Homepage hp = new Homepage(driver);              //To create Home Page object
+	hp.resourcebutton();                             //Referece of Home page object is use to call resourcebutton method
 	
-	Guidepage gp = new Guidepage(driver);
+	Guidepage gp = new Guidepage(driver);              //To create Guide Page object
 	
 	String actual = gp.getblogvalidation();
 	
-	Assert.assertEquals(actual, "Resource Center");
+	Assert.assertEquals(actual, "Resource Center");      //Assertion is used to validatd "Resource Center" text present on guide page
 	}
 	
 	

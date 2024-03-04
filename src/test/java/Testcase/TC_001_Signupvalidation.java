27b337016f31d86d673738871931791f2e84e7be
sayeetale <145047252+sayeetale@users.noreply.github.com>
2024-03-04 23:44:01 +0530

    Update TC_001_Signupvalidation.java

--- src/test/java/Testcase/TC_001_Signupvalidation.java
+++ src/test/java/Testcase/TC_001_Signupvalidation.java
@@ -8,13 +8,13 @@ public class TC_001_Signupvalidation extends start {
 	@Test
 	public void signup()
 	{
-	Homepage hp = new Homepage(driver); //
-	hp.clickonsignupbutton();
+	Homepage hp = new Homepage(driver);                                  //To create Home page object
+	hp.clickonsignupbutton();                                           //Reference of Homepage object is use to call clickonsignupbutton method
 	
-	Signinpage sp = new Signinpage(driver);
+	Signinpage sp = new Signinpage(driver);                             //To create Signin page object
 	
-	String actualsignintitle = sp.getsignintitle();
-	Assert.assertEquals(actualsignintitle, "Entrata Sign In");
+	String actualsignintitle = sp.getsignintitle();                     //The return output value of getsignintitle method is stored in actualsignintitle
+	Assert.assertEquals(actualsignintitle, "Entrata Sign In");         //Assertion is use to validate expected title of signin page with actual title of signin page
 
 }
 }

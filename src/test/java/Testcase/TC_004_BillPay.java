4383a0585fffc891c93109bd22faefbc96157171
sayeetale <145047252+sayeetale@users.noreply.github.com>
2024-03-05 00:34:23 +0530

    Update TC_004_BillPay.java

--- src/test/java/Testcase/TC_004_BillPay.java
+++ src/test/java/Testcase/TC_004_BillPay.java
@@ -7,27 +7,24 @@ import PageObject.Homepage;
 import Testbase.start;
 
 public class TC_004_BillPay extends start {
-
 	
     @Test
-	public void bill_pay() throws InterruptedException
+      public void bill_pay() throws InterruptedException
 	{
 		Homepage hp = new Homepage(driver);                                        //To create Home Page object
-		hp.clickonacceptallcookiesbutton();
-		Thread.sleep(1000);
-		hp.bill_pay();
+		hp.clickonacceptallcookiesbutton();                                        //Referece of Home Page object is use to call clickonacceptallcookiesbutton method
+		Thread.sleep(1000);                                                        //Thread.sleep statement is use to wait after accepting the cookies
+		hp.bill_pay();                                                             //Referece of Home Page object is use to call bill_pay method
 		
-		Billpaypage bpp = new Billpaypage(driver);
-		String actualbillpaytext = bpp.getbillpayvalidation();
+		Billpaypage bpp = new Billpaypage(driver);                                  //To create Bill Pay Page object                       
+		String actualbillpaytext = bpp.getbillpayvalidation();                          
 		
-		Assert.assertEquals(actualbillpaytext, "Bill Pay");
+		Assert.assertEquals(actualbillpaytext, "Bill Pay");                          //Assertion is used to validatd "Bill Pay" text present on Bill Pay page
+                        
+		BillpageScheduledemopage bsp = new BillpageScheduledemopage(driver);          //To create Schedule demo Page object
+		bsp.getScheduledemourl();                                                     //Referece of Schedule demo Page object is use to call getScheduledemourl method                                          
 		
-		BillpageScheduledemopage bsp = new BillpageScheduledemopage(driver);
-		bsp.getScheduledemourl();
-		
-		String url = bsp.getScheduledemourl();
-		System.out.println("URL of Bill Pay Page is " + url);
+		String url = bsp.getScheduledemourl();                                         //get the URL of Schedule demo Page
+		System.out.println("URL of Bill Pay Page is " + url);                          //Print URL
 	}
-	
-    
 }

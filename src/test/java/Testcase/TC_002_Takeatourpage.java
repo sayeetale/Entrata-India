d30d7f0f2dbcce78970fc01a859fa4390ea1a8e2
sayeetale <145047252+sayeetale@users.noreply.github.com>
2024-03-05 00:07:21 +0530

    Update TC_002_Takeatourpage.java

--- src/test/java/Testcase/TC_002_Takeatourpage.java
+++ src/test/java/Testcase/TC_002_Takeatourpage.java
@@ -12,14 +12,14 @@ public class TC_002_Takeatourpage extends start {
    @Test
 	public void takeatour() throws InterruptedException 
 	{
-		Homepage hp = new Homepage(driver);
-		hp.clickonacceptallcookiesbutton();
-		Thread.sleep(3000);
-		hp.take_a_tour_button();
+		Homepage hp = new Homepage(driver);                         //To create Home Page object
+		hp.clickonacceptallcookiesbutton();                         //Referece of Home Page object is use to call clickonacceptallcookiesbutton method
+		Thread.sleep(3000);                                         //Thread.sleep statement is use to wait after accepting the cookies
+		hp.take_a_tour_button();                                    //Referece of Home Page object is use to call take_a_tour_button method
 		
-		Takeatourpage tp = new Takeatourpage(driver);
+		Takeatourpage tp = new Takeatourpage(driver);                //To create Take a Tour page object
 		
-		tp.myFirstName();
+		tp.myFirstName();                                            //Referece of Take a Tour page object is use to call methods present in Takeatourpage class
 		tp.myLastName();
 		tp.myEmailAddress();
 		tp.myCompanyName();
@@ -28,7 +28,7 @@ public class TC_002_Takeatourpage extends start {
 		tp.myJobTitle();
 		tp.myStatus();
 		
-		boolean actual = tp.watchdemobutton();
-		Assert.assertTrue(actual);
+		boolean actual = tp.watchdemobutton();                       
+		Assert.assertTrue(actual);                                   //To Validate Watch Demo button on Take a tour page
 	}
 }
